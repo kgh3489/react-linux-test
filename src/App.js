996b8011cffb89187d6653b5d1b01c86fc7b666c
kgh3489 <kgh3489@naver.com>
2022-11-25 12:37:47 +0900

    axios get

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import PrintMyName from './component/PrintMyName';
+import logo from "./logo.svg";
+import "./App.css";
+import PrintMyName from "./component/PrintMyName";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const getUsers = async() => {
+    const {data} = await axios.get("http://192.168.0.14:5000/user")
+    setUsers(data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <PrintMyName name="크리스티아누 호날두1"></PrintMyName>
-        <PrintMyName name="크리스티아누 호날두2"></PrintMyName>
+        <button onClick={getUsers}>getUsers</button>
+        {users.map((el) => (
+          <PrintMyName name={el.name}></PrintMyName>
+        ))}
+      
       </header>
     </div>
   );
